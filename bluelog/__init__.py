1f6cfe5a8e266be7bd9e291787c9a0e4eefba93b
ChenQianchenga <980938541@qq.com>
2023-10-04 23:04:24 +0800

    数据库模型创建

--- bluelog/__init__.py
+++ bluelog/__init__.py
@@ -7,12 +7,14 @@
 # Date:2023/10/3  16:32
 # --------------------------------------------------------------------------
 import os
+import click
 from flask import Flask, render_template
 from bluelog.blueprints import blog, admin, auth
 from bluelog.blueprints.admin import admin_bp
 from bluelog.blueprints.auth import auth_bp
 from bluelog.blueprints.blog import blog_bp
 from bluelog.extensions import bootstrap, db, ckeditor, mail, moment
+from bluelog.models import Admin, Category
 from bluelog.settings import config
 
 
@@ -68,7 +70,29 @@ def register_errors(app):
 
 
 def register_commands(app):
-    pass
+    @app.cli.command()
+    @click.option('--category', default=10, help='Quantity of categories, default is 10.')
+    @click.option('--post', default=50, help='Quantity of posts, default is 50.')
+    @click.option('--comment', default=500, help='Quantity of comments, default is 500.')
+    def forge(category, post, comment):
+        """Generates the fake categories, posts, and comments."""
+        from bluelog.fakes import fake_admin, fake_categories, fake_posts, fake_comments
+
+        db.drop_all()
+        db.create_all()
+
+        click.echo('Generating the administrator...')
+        fake_admin()
+
+        click.echo('Generating %d categories...' % category)
+        fake_categories(category)
+
+        click.echo('Generating %d posts...' % post)
+        fake_posts(post)
+        click.echo('Generating %d comments...' % comment)
+        fake_comments(comment)
+
+        click.echo('Done.')
 
 
 def register_shell_context(app):
