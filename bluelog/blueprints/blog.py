af09ee2e6932c9841a0e8660f83b2655ce4a0127
ChenQianchenga <980938541@qq.com>
2023-10-12 23:46:24 +0800

    显示分类文章列表

--- bluelog/blueprints/blog.py
+++ bluelog/blueprints/blog.py
@@ -6,11 +6,48 @@
 # Author:ChenQiancheng
 # Date:2023/10/3  16:07
 # --------------------------------------------------------------------------
-from flask import render_template, flash, redirect, url_for, Blueprint
+from flask import render_template, flash, redirect, url_for, Blueprint, request, current_app
+from bluelog.models import Post, Category
 
 blog_bp = Blueprint('blog', __name__)
 
 
-@blog_bp.route('/')
-def index():
-    return "hello world"
+@blog_bp.route('/', defaults={'page': 1})
+@blog_bp.route('/page/<int:page>')
+def index(page):
+    """
+    获取所有文章分页展示
+    :param page: 页码
+    :return: 返回当前页码的所有文章
+    """
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['BLUELOG_POST_PER_PAGE'])
+    posts = pagination.items
+    return render_template('blog/index.html', pagination=pagination, posts=posts)
+
+
+# @blog_bp.route('/post/<slug>')
+# def show_post(slug):
+#     post = Post.query.filter_by(slug=slug).first_or_404()
+#     return render_template('post.html', post=post)
+
+
+@blog_bp.route('/about')
+def about():
+    return render_template('blog/about.html')
+
+
+@blog_bp.route('/category/<int:category_id>')
+def show_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['BLUELOG_POST_PER_PAGE']
+    pagination = Post.query.with_parent(category).order_by(Post.timestamp.desc()).paginate(page, per_page=per_page)
+    posts = pagination.items
+    return render_template('blog/category.html', category=category, pagination=pagination, posts=posts)
+
+
+@blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('blog/post.html', post=post)
--- bluelog/utils.py
+++ bluelog/utils.py
@@ -1,8 +1,20 @@
-# -*- coding: utf-8 -*-#
-# --------------------------------------------------------------------------
-# ProjectName：bluelog
-# Name:utils.py
-# Description: 用来存储各种辅助函数
-# Author:ChenQiancheng
-# Date:2023/10/3  16:04
-# --------------------------------------------------------------------------
+import re
+from unidecode import unidecode
+
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+
+def slugify(text, delim='-'):
+    """生成仅包含ASCII字符的slug。"""
+    result = []
+    for word in _punct_re.split(text.lower()):
+        print(word, end='&')
+        result.extend(unidecode(word).lower().split())
+        print(result)
+    return delim.join(result)
+
+
+if __name__ == '__main__':
+    text = "邻家的豆豆龙"
+    slug = slugify(text)
+    print(slug)  # 输出: ni-hao-shi-jie-ni-hao-ma
